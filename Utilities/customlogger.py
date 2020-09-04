80902efe4fb5b0ee1cbf0f3c34f5316660326bd9
KP-crypto <puranik324@gmail.com>
2020-09-04 07:55:51 +0530

    this is third commit

--- Utilities/customlogger.py
+++ Utilities/customlogger.py
@@ -3,7 +3,7 @@ import logging
 class LogGen:
     @staticmethod
     def loggen():
-        logging.basicConfig(filename="..\\Logs\\automation.log",
+        logging.basicConfig(filename="C:/Users/Acer/PycharmProjects/wpmobil/Logs/ketan.logs",
                             format='%(asctime)s %(levelname)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 
         logger = logging.getLogger()
